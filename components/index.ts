0e50aabb75fd36126e6377c42a524b5fdee59f62
Pham Nguyen Thai Khuong <thaikhuong62000@gmail.com>
2025-05-18 15:13:29 +0700

    clean: Organize file

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,3 @@
 export * from "./App";
-export * from "./CameraModal";
-export * from "./Message";
-export * from "./Screen";
+export * from "./common";
+export * from "./messenger";
---
+++ components/messenger/index.ts
@@ -0,0 +1,2 @@
+export * from "./CameraModal";
+export * from "./Message";
